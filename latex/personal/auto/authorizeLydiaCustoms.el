648ccbecd056d55dd0b6d5f2c73d22c571441f35
fenton travers <fenton.travers@gmail.com>
2010-11-11 19:40:35 +0800

    .

---
+++ latex/personal/auto/authorizeLydiaCustoms.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook "authorizeLydiaCustoms"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "letter12"
+     "letter"
+     "final"
+     "letterpaper"
+     "12pt")))
+
---
+++ latex/rentalProperty/auto/evictionLetter.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook "evictionLetter"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "letter12"
+     "letter"
+     "final"
+     "letterpaper"
+     "12pt")))
+
