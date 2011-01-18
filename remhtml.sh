887c3f1e4d1412e16a233773c668b0dea7762542
Bruno Vanderstocken <bruno.vanderstocken@oracle.com>
2011-01-18 15:05:02 +0800

    .

--- remhtml.sh
+++ remhtml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/bin/website
-time rsync -avP . ftravers@spicevan.com:/home/ftravers/mohe.spicevan.com 
-time rsync -avP . root@linux1.hk.oracle.com:/var/www/html/docs/
+#time rsync -avP . ftravers@spicevan.com:/home/ftravers/mohe.spicevan.com 
+time rsync -avP . root@linux1.hk.oracle.com:/var/www/html/
