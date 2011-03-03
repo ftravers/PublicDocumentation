73cfb3dc81c4ed40034936612ebd6c6f4244bf40
Fenton Travers <fenton.travers@gmail.com>
2011-03-03 18:48:50 +0800

    .

--- multimarkdown.sh
+++ multimarkdown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
-
 for filename in `find . -name \*.mmd`; do
-    NAME=`echo $filename | sed -e 's/\.md//g'`
+    NAME=`echo $filename | sed -e 's/\.mmd//g'`
 	multimarkdown2XHTML.pl $NAME.mmd > $NAME.html    
 done
 
