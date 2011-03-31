d9e1923424c6a2f0c76ac92c48ecc01df66dcc15
Fenton Travers <fenton.travers@gmail.com>
2011-03-31 10:33:20 +0800

    .

--- remhtml.sh
+++ remhtml.sh
@@ -2,3 +2,4 @@
 cd $HOME/bin/website
 #time rsync -avP . ftravers@spicevan.com:/home/ftravers/mohe.spicevan.com 
 time rsync -avP . root@linux1.hk.oracle.com:/var/www/html/
+time rsync -avP . ftravers@spicevan.com:/home/ftravers/spicevan.com/current/public
