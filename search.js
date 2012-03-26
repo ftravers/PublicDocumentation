1ab481889b0e9ca9906209075c58189cc9d0196e
Fenton Travers <fenton.travers@oracle.com>
2012-03-26 14:39:53 -0700

    .

--- search.js
+++ search.js
@@ -1,4 +1,5 @@
 var searching = true
+
 function load_content(content_name) {
     searching = false
     ajaxRequest.open("GET", content_name + ".html", true);
@@ -7,11 +8,23 @@ function load_content(content_name) {
 function set_details_box(body) {
     var details_box = document.getElementById('details');
     details_box.innerHTML = body   
-}
+    var searchResultsDiv = document.getElementById('results');
+    searchResultsDiv.style.display = "none"
+    details_box.style.display = "block"
+    var searchAgainButton = document.getElementById('search_again_button');
+    searchAgainButton.style.display = "inline"
 
+}
 function setSearchResults(body) {
-		var ajaxDisplay = document.getElementById('results');
-		ajaxDisplay.innerHTML = body;    
+    var details_box = document.getElementById('details');
+    details_box.style.display = "none"
+
+		var searchResultsDiv = document.getElementById('results');
+    searchResultsDiv.style.display = "block"
+
+		searchResultsDiv.innerHTML = body;    
+    var searchAgainButton = document.getElementById('search_again_button');
+    searchAgainButton.style.display = "none"
 }
 
 ajaxRequest = new XMLHttpRequest();
@@ -25,17 +38,22 @@ ajaxRequest.onreadystatechange = function(){
             var ajax_loader = document.getElementById('ajaxLoader');    
             ajax_loader.innerHTML = "";
         }
-        else { set_details_box(response) }            
+        else { 
+            set_details_box(response)
+        } 
 		}
 }
 
-function doSearch(input) {
+function doSearch() {
+    var search_box = document.getElementById('search_input')
     searching = true
-    text = input.value
+    text = search_box.value
+
     if ( !text ) {
         setSearchResults("");
         return;
     }
+
     var ajax_loader = document.getElementById('ajaxLoader');
     ajax_loader.innerHTML = "<img src='ajax-loader.gif'/>";
 	  ajaxRequest.open("GET", "/srch?query=" + text, true);
